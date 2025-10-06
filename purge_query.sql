1ba5b3c10df8b84f6b4e333a918f099b9aed956d
Daniel Dixon <112588576+Rockyroad1827@users.noreply.github.com>
2025-10-06 09:27:04 +0100

    Add files via upload

---
+++ purge_query.sql
@@ -0,0 +1,28 @@
+
+
+-- Show The Current Deleted items before deletion
+SELECT *
+FROM Database_name.deletions;
+
+
+-- Permanent Deletion Query:
+DELETE
+FROM Database_name.deletions
+WHERE deleted_by = UserID;
+
+--Delete Audit Log
+DELETE
+FROM Database_name.activities
+Where user_id = UserID;
+
+-- Delete "Deleted items link" from the recycle bin
+DELETE
+FROM Database_name.chapters
+WHERE deleted_at IS NOT NULL
+AND owned_by = UserID;
+
+-- Delete "Deleted items link" from the recycle bin
+DELETE
+FROM Database_name.pages
+WHERE deleted_at IS NOT NULL
+AND owned_by = UserID;
